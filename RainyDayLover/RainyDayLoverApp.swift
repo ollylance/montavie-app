15e56a4a4f1a9fb69337ee45b55bb41b6a40a88b
Oliver Lance <42589078+ollylance@users.noreply.github.com>
2022-11-12 12:51:05 -0500

    Initial Commit

---
+++ RainyDayLover/RainyDayLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainyDayLoverApp.swift
+//  RainyDayLover
+//
+//  Created by Oliver Lance on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RainyDayLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
