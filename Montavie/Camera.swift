7e8e3e9e9d116458fe2330f4c9bf1707b0e6de45
Oliver Lance <42589078+ollylance@users.noreply.github.com>
2023-02-10 17:17:25 -0500

    committing project files
    did not work with versioning during development so this contains a lot of files.

---
+++ Montavie/Camera.swift
@@ -0,0 +1,8 @@
+//
+//  Camera.swift
+//  RainyDayLover
+//
+//  Created by Oliver Lance on 11/13/22.
+//
+
+import Foundation
---
+++ Montavie/Secrets.swift
@@ -0,0 +1,10 @@
+//
+//  Secrets.swift
+//  RainyDayLover
+//
+//  Created by Oliver Lance on 1/25/23.
+//
+
+import Foundation
+
+let teamID = "Q8YD26T473"
